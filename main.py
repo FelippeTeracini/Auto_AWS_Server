d834d1ef1556a05c5546ed78905a5c87c8cb78c7
Felippe Nalin Teracini <fe.n.teracini@hotmail.com>
2019-11-14 11:00:43 -0800

    all public

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ SECURITY_GROUP_NAME = "TeraSecurityGroup"
 TARGET_GROUP_NAME = "TeraTargetGroup"
 LOAD_BALANCER_NAME = "TeraLoadBalancer"
 IMAGE_NAME = "TeraImage"
+LAUNCH_CONFIGURATION_NAME = "TeraLaunchConfiguration"
+AUTOSCALING_NAME = "TeraAutoscaling"
+
+MIN_AUTOSCALING_SIZE = 1
+MAX_AUTOSCALING_SIZE = 3
+
+INSTANCE_TYPE = "t2.micro"
 
 WEBSERVER_PORT = 5000
 
@@ -43,30 +50,9 @@ def terminate_instances():
                 ec2.instances.filter(InstanceIds=[ids[i]]).terminate()
                 print("Instance deleted")
 
-        # terminated = False
-        # while(not terminated):
-        #     ids = []
-        #     states = []
-        #     terminated = True
-
-        #     response = client.describe_instances(
-        #         Filters=[
-        #             {
-        #                 'Name': 'tag:Owner',
-        #                 'Values': [
-        #                     'Tera',
-        #                 ]
-        #             }
-        #         ]
-        #     )
-        #     for i in response["Reservations"]:
-        #         ids.append(i["Instances"][0]["InstanceId"])
-        #         states.append(i["Instances"][0]["State"]["Name"])
-
-        #     for i in range(len(ids)):
-        #         if states[i] != "terminated":
-        #             terminated = False
-
+        if(len(ids)) <= 0:
+            print("Sem instancias")
+            return
         waiter = client.get_waiter('instance_terminated')
         waiter.wait(
             Filters = [{
@@ -76,6 +62,7 @@ def terminate_instances():
                 ]
             }]
         )
+        
         print("Instance terminated")
 
     except ClientError as e:
@@ -152,7 +139,7 @@ def create_instance():
         MaxCount=1,
         SecurityGroups=[SECURITY_GROUP_NAME],
         KeyName=KEY_PAIR_NAME,
-        InstanceType="t2.micro",
+        InstanceType=INSTANCE_TYPE,
         TagSpecifications=[{'ResourceType': 'instance',
                             'Tags': [{'Key': 'Owner', 'Value': 'Tera'}, {'Key': 'Name', 'Value': 'TeraWebserverProjeto'}]}],
         UserData='''#! /bin/bash
@@ -163,17 +150,17 @@ def create_instance():
                     pip3 install pydantic
                     cd home/ubuntu
                     git clone https://github.com/FelippeTeracini/Mini_REST_Tasks.git
-                    cd Mini_REST_Tasks/
-                    uvicorn main:app --reload --host "0.0.0.0" --port 5000
+                    cd Mini_REST_Tasks
+                    uvicorn main:app --reload --host "0.0.0.0" --port {}
                         '''
     )
     print("Instances Created")
     instance_ids = []
     for instance in instances:
         instance_ids.append(instance.id)
-    waiter = client.get_waiter('instance_running')
+    waiter = client.get_waiter('instance_status_ok')
     waiter.wait(InstanceIds = instance_ids)
-    print("Instances Running")
+    print("Instances Running and Status OK")
 
 def delete_target_group():   
     try:
@@ -196,7 +183,8 @@ def create_target_group():
         Protocol='HTTP',
         Port=WEBSERVER_PORT,
         VpcId=vpc_id,
-        TargetType='instance'
+        TargetType='instance',
+        HealthCheckEnabled=True
     )
     arn = response['TargetGroups'][0]['TargetGroupArn']
     print("Target Group Created")
@@ -210,7 +198,7 @@ def delete_load_balancer():
             response = elbv2.delete_load_balancer(LoadBalancerArn=arn)
             waiter = elbv2.get_waiter('load_balancers_deleted')
             waiter.wait(LoadBalancerArns = [arn])
-            time.sleep(20)
+            time.sleep(30)
             print("Load Balancer Deleted")
         except ClientError as e:
             print(e)
@@ -272,10 +260,12 @@ def create_image():
             instance_id = i["Instances"][0]["InstanceId"]
 
     response = client.create_image(InstanceId=instance_id, Name=IMAGE_NAME)
+    image_id = response['ImageId']
     print("Image Created")
     waiter = client.get_waiter('image_available')
-    waiter.wait(ImageIds= [response['ImageId']])
+    waiter.wait(ImageIds= [image_id])
     print("Image Available")
+    return image_id
 
 def deregister_image():
     try:
@@ -308,7 +298,108 @@ def create_listener(lb_arn, tg_arn):
     )
     print("Listener Created")
 
+def create_launch_configuration(image_id):
+    response = autoscaling.create_launch_configuration(
+        LaunchConfigurationName=LAUNCH_CONFIGURATION_NAME,
+        ImageId=image_id,
+        KeyName=KEY_PAIR_NAME,
+        SecurityGroups=[SECURITY_GROUP_NAME],
+        InstanceType = INSTANCE_TYPE,
+        InstanceMonitoring={'Enabled': True},
+        UserData = '''#!/bin/bash
+                    cd home/ubuntu/Mini_REST_Tasks
+                    uvicorn main:app --reload --host "0.0.0.0" --port {}
+        '''.format(WEBSERVER_PORT)
+    )
+    print("Launch Configuration Created")
+
+def create_launch_configuration2():
+    response = autoscaling.create_launch_configuration(
+        LaunchConfigurationName=LAUNCH_CONFIGURATION_NAME,
+        ImageId='ami-04b9e92b5572fa0d1',
+        KeyName=KEY_PAIR_NAME,
+        SecurityGroups=[SECURITY_GROUP_NAME],
+        InstanceType = INSTANCE_TYPE,
+        InstanceMonitoring={'Enabled': True},
+        UserData = '''#! /bin/bash
+sudo apt-get update
+sudo apt-get -y install python3-pip
+pip3 install fastapi
+pip3 install uvicorn
+pip3 install pydantic
+cd home/ubuntu
+git clone https://github.com/FelippeTeracini/Mini_REST_Tasks.git
+cd Mini_REST_Tasks
+uvicorn main:app --reload --host "0.0.0.0" --port {}
+        '''.format(WEBSERVER_PORT)
+    )
+    print("Launch Configuration Created")
+    
+def delete_launch_configuration():
+    try:
+        response = autoscaling.delete_launch_configuration(LaunchConfigurationName=LAUNCH_CONFIGURATION_NAME)
+        print("Launch Configuration Deleted")
+    except ClientError as e:
+        print(e)
+
+def create_autoscaling(tg_arn):
+    response = autoscaling.create_auto_scaling_group(
+        AutoScalingGroupName=AUTOSCALING_NAME,
+        LaunchConfigurationName=LAUNCH_CONFIGURATION_NAME,
+        MinSize=MIN_AUTOSCALING_SIZE,
+        MaxSize=MAX_AUTOSCALING_SIZE,
+        DefaultCooldown = 20,
+        DesiredCapacity=MIN_AUTOSCALING_SIZE,
+        AvailabilityZones=[
+            'us-east-1a',
+            'us-east-1b',
+            'us-east-1c',
+            'us-east-1d',
+            'us-east-1e',
+            'us-east-1f',
+        ],
+        TargetGroupARNs=[
+            tg_arn,
+        ],
+        Tags=[
+            {
+                'Key': 'tag:Owner',
+                'Value': 'Tera'
+            }
+        ]
+    )
+    print("Autoscaling Created")
+
+def delete_autoscaling():
+    try:
+        response = autoscaling.update_auto_scaling_group(
+            AutoScalingGroupName=AUTOSCALING_NAME,
+            MinSize=0,
+            MaxSize=0,
+            DesiredCapacity=0,
+        )
+        print("Autoscaling Updated")
+        response = autoscaling.delete_auto_scaling_group(
+            AutoScalingGroupName=AUTOSCALING_NAME,
+            ForceDelete=True
+        )
+        print("Autoscaling Deleted")
+        deleted = False
+        while(not deleted):
+            response = autoscaling.describe_auto_scaling_groups(
+                AutoScalingGroupNames=[
+                    AUTOSCALING_NAME
+                ]
+            )
+            if(len(response['AutoScalingGroups']) <= 0):
+                deleted = True
+        print("Autoscaling and Instances Deleted")
+    except ClientError as e:
+        print(e)
+
 def main():
+    print("----- MAIN -----")
+    delete_autoscaling()
     terminate_instances()
     delete_load_balancer()
     delete_target_group()
@@ -316,13 +407,48 @@ def main():
     delete_key_pair()
     create_key_pair()
     delete_security_group()
+    delete_launch_configuration()
     create_security_group()
     create_instance()
-    create_image()
+    image_id = create_image()
+    terminate_instances()
     lb_arn = create_load_balancer()
     tg_arn = create_target_group()
     create_listener(lb_arn, tg_arn)
+    create_launch_configuration(image_id)
+    create_autoscaling(tg_arn)
 
+def create_north_virginia():
+    print("----- create_north_virginia-----")
+    delete_autoscaling()
+    terminate_instances()
+    delete_load_balancer()
+    delete_target_group()
+    delete_key_pair()
+    create_key_pair()
+    delete_security_group()
+    delete_launch_configuration()
+    create_security_group()
+    lb_arn = create_load_balancer()
+    tg_arn = create_target_group()
+    create_listener(lb_arn, tg_arn)
+    create_launch_configuration2()
+    create_autoscaling(tg_arn)
 
-main()
+def delete_all():
+    delete_autoscaling()
+    terminate_instances()
+    delete_load_balancer()
+    delete_target_group()
+    deregister_image()
+    delete_key_pair()
+    delete_security_group()
+    delete_launch_configuration()
+
+def create_test_instance():
+    create_instance()
+
+create_test_instance()
+# create_north_virginia()
+# print(autoscaling.waiter_names)
 # print(client.waiter_names)
